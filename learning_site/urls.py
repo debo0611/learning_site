d3aa8215d55b8ca58750efaf53cd8f9a12bda7bf
Debojyoti <debojyoti.pramanick@gmail.com>
2016-09-25 10:21:54 +0000

    first commit from Treehouse workspaces

---
+++ learning_site/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from . import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'learning_site.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.hello_world),
+]
